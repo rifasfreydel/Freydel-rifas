394577cddd5236c757f3e002a3661057e6c2b44c
rifasfreydel <rifasfreydel@gmail.com>
2025-09-10 17:27:47 -0400

    Update login.js

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,16 +1,12 @@
 import { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from "next/router";
-import { app } from "../../lib/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../lib/firebase";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-
-  const auth = getAuth(app);
-  const router = useRouter();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -19,7 +15,7 @@ export default function LoginPage() {
 
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      router.push("/admin/dashboard"); // redirige con Next.js
+      window.location.href = "/admin/dashboard"; // redirigir
     } catch (err) {
       setError("Usuario o contraseña incorrectos");
       console.error(err);
@@ -30,10 +26,7 @@ export default function LoginPage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form
-        onSubmit={handleLogin}
-        className="bg-white p-6 rounded-lg shadow-md w-80"
-      >
+      <form onSubmit={handleLogin} className="bg-white p-6 rounded-lg shadow-md w-80">
         <h2 className="text-2xl font-bold mb-4 text-center">Login Admin</h2>
 
         {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
